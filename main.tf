06da3e374efc81c006a14ee641cf2d1c60584624
Rapalli Krishna Mallikharjun <rapalli.krish.1998@gmail.com>
2026-07-18 18:23:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_template" "web_server_as" {
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
      listener {
-      instance_port     = 8000
+      instance_port     = 80
       instance_protocol = "http"
       lb_port           = 80
       lb_protocol       = "http"
